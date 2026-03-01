b9ee8f79af14b8df420fac6c9f145bf6dea3aded
Dana0507 <146192753+Dana0507@users.noreply.github.com>
2026-03-01 19:45:25 +0200

    updated

--- backend/app/Models/Campus.php
+++ backend/app/Models/Campus.php
@@ -2,18 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Campus extends Model
 {
-    use HasFactory;
-
     protected $table = 'campuses';
-    protected $fillable = ['name', 'university_id'];
+
+    protected $primaryKey = 'campus_id';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'campus_name',
+        'university_id'
+    ];
 
     public function university()
     {
-        return $this->belongsTo(University::class);
+        return $this->belongsTo(University::class, 'university_id', 'university_id');
     }
 }
